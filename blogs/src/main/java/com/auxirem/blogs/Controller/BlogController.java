0d4adb6062bf96a3952d0c57431e1fc2d7f757cb
naykalritesh <naykalritesh284@gmail.com>
2025-09-20 16:34:32 +0530

    Logger is added

--- blogs/src/main/java/com/auxirem/blogs/Controller/BlogController.java
+++ blogs/src/main/java/com/auxirem/blogs/Controller/BlogController.java
@@ -2,6 +2,8 @@ package com.auxirem.blogs.Controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,10 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.auxirem.blogs.Model.Blog;
 import com.auxirem.blogs.Services.BlogService;
 
-@CrossOrigin (origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/blogs")
 public class BlogController {
+    private static final Logger logger = LoggerFactory.getLogger(BlogController.class);
 
     @Autowired
     private BlogService blogService;
@@ -27,31 +30,35 @@ public class BlogController {
     // Get all blogs
     @GetMapping("/all")
     public List<Blog> getAllBlogs() {
-
+        logger.info("Fetching all blogs");
         return blogService.getAllBlogs();
     }
 
     //Get Blog by id
     @GetMapping("/{id}")
     public Blog getBlogById(@PathVariable Long id){
+        logger.info("Fetching blog with Id:",id);
         return blogService.getBlogById(id);
     }
 
     // create new blog
     @PostMapping("/add")
     public Blog createNewBlog(@RequestBody Blog blog){
-        System.out.println(blog.getThumbnailImage());
+        logger.info("Creating new blog with title:",blog.getTitle());
+        logger.debug("Thumbnail image:",blog.getThumbnailImage());
         return blogService.createNewBlog(blog);
     }
         
     //update blog
     @PutMapping("/{id}")
     public Blog updateBlog(@PathVariable Long id,@RequestBody Blog blog){
+        logger.info("updating blog with id:", id);
         return blogService.updateBlog(id, blog);
     }
      
     @DeleteMapping("/{id}")
     public String deleteBlog(@PathVariable Long id){
+        logger.info("Deleting blog with id:", id);
         blogService.deleteBlog(id);
         return "Blog with ID " + id + " deleted successfully.";
     }
--- blogs/src/main/java/com/auxirem/blogs/Services/BlogService.java
+++ blogs/src/main/java/com/auxirem/blogs/Services/BlogService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +14,24 @@ import com.auxirem.blogs.Repository.BlogRepository;
 
 @Service
 public class BlogService {
+  private static final Logger logger = LoggerFactory.getLogger(BlogService.class);
 
     @Autowired
     private BlogRepository blogRepository;
 
     // Get all blogs
     public List<Blog> getAllBlogs() {
+        logger.info("Fetching all blogs from repository");
         return blogRepository.findAll();
     }
     public Blog getBlogById(Long id) {
+        logger.info("Fetching blog with id:",id);
         Optional<Blog> existingBlog = blogRepository.findById(id);
         if (existingBlog.isPresent()) {
+            logger.info("Blog found with id:", id);
            return  existingBlog.get();
         }else{
-           
+            logger.info("Blog not found with id:", id);
             return null;
            
         }
@@ -35,10 +41,12 @@ public class BlogService {
     public Blog createNewBlog(Blog blog){
         blog.setCreated_On(LocalDateTime.now());
         blog.setUpdated_On(LocalDateTime.now());
+        logger.info("Creating new blog with title:", blog.getTitle());
         return  blogRepository.save(blog);
     }
 
     public Blog updateBlog(Long id, Blog updatedBlog) {
+        logger.info("Updating blog with id:", id);
        return blogRepository.findById(id).map(blog -> {
             blog.setTitle(updatedBlog.getTitle());
             blog.setContent(updatedBlog.getContent());
@@ -51,12 +59,17 @@ public class BlogService {
             blog.setMetaTitle(updatedBlog.getMetaTitle());
             blog.setMetaDescription(updatedBlog.getMetaDescription());
             blog.setUpdated_On(LocalDateTime.now());
+            logger.info("Blog updated successfully with id:", id);
             return blogRepository.save(blog);
-        }).orElseThrow(() -> new RuntimeException("Blog not found with id " + id));
+        }).orElseThrow(() -> {
+            logger.error("Blog not found with id:", id);
+            return new RuntimeException("Blog not found with id " + id);});
     }
 
     public void deleteBlog(Long id) {
-         blogRepository.deleteById(id);
+        logger.info("Deleting blog with id:",id);
+        blogRepository.deleteById(id);
+        logger.info("Blog deleted successfully with id:", id);
     }
 
     
